4b1b51ee205bc3f1e5c5eb5a6b54c82d99258ded
Syed Aman <sna274@nyu.edu>
2016-11-15 20:15:17 -0500

    call function manually instead of using =>

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.get('/', response => response.send('Express Works'));
+app.get('/', function(request, response){
+    response.send('Express Works');
+});
 app.listen(app.get('port'), console.log('Express started. Press CTRL + C to terminate'));
